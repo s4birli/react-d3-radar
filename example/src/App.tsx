405c9e697d7e48e85cedbc14b9847547ed051d4f
s4birli <sabirli31@gmail.com>
2021-11-16 15:02:32 +0900

    initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import SayHello from 'react-d3-radar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name={'adsf'} />
+    </div>
+  );
+}
+
+export default App;
